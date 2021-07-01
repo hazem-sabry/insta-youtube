77005a39a1b22139c27126329eab9e64d5ca49fa
hazem-sabry <hazem.career@gmail.com>
2021-07-01 15:08:00 +0200

    feat: create our main api endpoints

---
+++ api/endpoints.js
@@ -0,0 +1,8 @@
+// Our Request Tuples are here.
+export const GET_CHANNEL = ['get', `/channels?part=snippet`]
+
+export const GET_PLAYLIST = ['get', `/channels?part=snippet`]
+
+export const GET_VIDEO = ['get', `/videos?part=contentDetails,statistics`]
+
+export const GET_SEARCH = ['get', `/search?part=snippet`]
