c026729de64a36c54f79f4a309f481ee479c47a2
hazem-sabry <hazem.career@gmail.com>
2021-07-02 18:57:48 +0200

    feat: create empty state components

---
+++ components/NoResultsFound.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="EmptyPage">
+    <svg-icon name="empty-page" class="EmptyPage__icon" />
+    <p>Nothing here so far...</p>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.EmptyPage {
+  width: 100%;
+  max-width: 28rem;
+  margin: 50px auto 0;
+  padding-inline: 15px;
+  text-align: center;
+
+  &__icon {
+    width: 100%;
+    height: 100%;
+    margin-bottom: 20px;
+  }
+}
+</style>
